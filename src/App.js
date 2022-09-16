1af5afa040d28e1c7f20402bf4f75c808a808a1d
mpsolares <marianasolares@gmail.com>
2022-09-16 19:15:17 -0300

    fixed some routing errors

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import Navbar from './components/NavBar/Navbar';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App({ props }) {
   return (
@@ -11,10 +12,8 @@ function App({ props }) {
       <div className='container-fluid m-0 p-0'>
       <Routes>
           <ItemListContainer />
-          {/* <Route path="/itemDetail" element={<ItemDetail/>} />
-          <Route path='/itemDetail/:id' element={<ItemDetail/>}/> */}
           <Route path='/' element={<ItemListContainer/>} />
-          <Route path='/:categoriaId' element={<ItemListContainer/>} />
+          <Route path='/:categoryId' element={<ItemListContainer/>} />
           <Route path='/item/:Id' element={<ItemDetailContainer/>} />
         </Routes>
       </div>
