1367501071e2613cb0b6017edb7245e74d526312
mpsolares <marianasolares@gmail.com>
2022-09-16 19:07:13 -0300

    Replaced all a links by NavLink

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import CartWidget from "../CartWidget/CartWidget";
 import brand from "../img/mps.svg";
 import './Navbar.css';
+import {NavLink} from "react-router-dom"
 
 function Navbar() {
     return (
         <div >
             <nav className="navbar navbar-expand-lg bg-link navbar-light ">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#"><img src={brand} alt="" height="40" className="d-inline-block align-text-center"/></a>
+                    <NavLink className="navbar-brand" to="home"><img src={brand} alt="" height="40" className="d-inline-block align-text-center"/></NavLink>
                     <button className="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <i className="fas fa-bars"></i>
                     </button>
@@ -16,13 +17,13 @@ function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link link_header" href="#">Home</a>
+                                <NavLink className="nav-link link_header" to="home">Home</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link link_header" href="#">Shop</a>
+                                <NavLink className="nav-link link_header" to="shop">Shop</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link link_header" href="#">Contact</a>
+                                <NavLink className="nav-link link_header" to="contact">Contact</NavLink>
                             </li>
                         </ul>
                         <div className="navbar-nav d-flex flex-row me-1">
