f8c799024545e6ea27771935aaee8cb35b1beadb
mpsolares <marianasolares@gmail.com>
2022-09-17 22:13:32 -0300

    some aesthetic changes

--- src/Mock.jsx
+++ src/Mock.jsx
@@ -1,5 +1,5 @@
 const Products = [
-    {id:1, name:"Lightweight Jacket", price:"32000", img:"product-01.jpg", description:"Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.", category:"men"},
+    {id:1, name:"Lightweight Jacket", price:"32000", img:"product-01.jpg", imgDetail1:"product-detail-01.jpg", imgDetail2:"product-detail-02.jpg", imgDetail3:"product-detail-03.jpg", description:"Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.", category:"men"},
     {id:2, name:"Herschel supply", price:"8500", img:"product-02.jpg", description:"Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat", category:"women"},
     {id:3, name:"Only Check Trouser", price:"52000", img:"product-03.jpg", description:"Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat.", category:"men"},
     {id:4, name:"Classic Trench Coat", price:"12500", img:"product-04.jpg", description:"Nulla eget sem vitae eros pharetra viverra. Nam vitae luctus ligula. Mauris consequat ornare feugiat", category:"women"}
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -10,20 +10,34 @@ export const ItemDetail = ({data}) => {
       }
 
 return(
-    <div className="container-fluid mt-4 pt-2">
-        <div className="row mt-4">
-            <div className="col-md-3 offset-md-3">
-                <img className="img-fluid" src={"../img/" + data.img} alt={data.name} />
+    <div className="container-fluid mt-2 pt-2">
+    <div className="row mt-4 d-flex justify-content-center">
+        <div className="col-md-1 m-0 offset-md-1">
+            <div class="d-flex align-items-end flex-column mb-3">
+                <div className="p-2 pt-0 ">
+                    <img className="img-preview" src={"../img/" + data.imgDetail1} alt={data.name}/>
+                </div>
+                <div className="p-2 ">
+                    <img className="img-preview" src={"../img/" + data.imgDetail2} alt={data.name}/>
+                </div>
+                <div className="p-2">
+                    <img className="img-preview" src={"../img/" + data.imgDetail3} alt={data.name}/>
+                </div>
             </div>
-            <div className="col-md-3">
-                <h3 className="mx-2">{data.name}</h3>
-                <p className="mx-2"><Link to={"/" + data.category} className="text-decoration-none"><i>{data.category}</i></Link></p>
-                <p className="mx-2">{data.description}</p>
-                <p className="mx-2 mb-3">${data.price}</p>
-                <ItemCount initial={1} stock={10} onAdd={onAdd} />
-            </div>
+            
+        </div>
+        <div className="col-md-5">
+            <img className="img-fluid" src={"../img/" + data.img} alt={data.name} />
+        </div>
+        <div className="col-md-5">
+            <h3 className="mx-2">{data.name}</h3>
+            <p className="mx-2"><Link to={"/" + data.category} className="text-decoration-none text-secondary"><i>{data.category}</i></Link></p>
+            <p className="mx-2">{data.description}</p>
+            <p className="mx-2 mb-3">${data.price}</p>
+            <ItemCount initial={1} stock={10} onAdd={onAdd} />
         </div>
     </div>
+</div>
 );
 }
 
