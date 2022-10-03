8646d53d78357f6dd9f7fe70f5fa86a120315fdd
mpsolares <marianasolares@gmail.com>
2022-10-02 21:35:25 -0300

    cart updates

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,46 +1,31 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Mock from "../../Mock";
-import Itemlist from "../ItemList/ItemList";  
-import Carousel from "../Carousel/Carousel";
-import Navbar from "../NavBar/Navbar";
-import { List } from "../List/List";
-import Footer from "../Footer/Footer";
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import ItemDetails from "./ItemDetails";
+import Loading from "../Loading/Loading";
 
-const ItemListContainer = () => {
-  const [products, setProducts] = useState([]);
-  const {categoryId} = useParams();
- 
-  useEffect(() =>{
-     const getData = new Promise((resolve =>{
-       setTimeout(() => {
-         resolve(Mock)
-       }, 500);
-     }));
-     if (categoryId) {
-       getData.then(res=>setProducts(res.filter(promise => promise.category === categoryId)));
-     }else{
-       getData.then(res=>setProducts(res));
-     }
-   },[categoryId])
-   
-  return(
-    <div className=''>
-      <Navbar />
-      <div className="background">
-          <Carousel />    
-      </div>  
-      <div className="container-fluid my-4 px-4">
-          <h2 className="mb-4 px-4">PRODUCT OVERVIEW</h2>
-          <List />
-          <Itemlist Prod = {products}/>
-      </div>
-      <Footer />
-   
-    </div>
-  )
-  }
+const ItemDetailsContainer = () => {
+  
+    const {id} = useParams();
+    const [item, setItem] = useState({});
+    const [loading, setLoading] = useState(true);
 
-export default ItemListContainer;
+    useEffect(() => {
+        const db = getFirestore();
+        const response = doc(db, "items", id);
+        getDoc(response).then((snapShot) => {
+            if (snapShot.exists()) {
+                setItem({id:snapShot.id, ...snapShot.data()});
+                setLoading(false);
+            }            
+        });
+    }, [id]);
+
+    return (
+        <div className="">
+            {loading ? <Loading /> : <ItemDetails item={item} />}
+        </div>
+    )
+}
+
+export default ItemDetailsContainer;
